a8d8dc775036ab3711207b318e5ecc254553b2a9
Zach Lesperance <zplesperance@gmail.com>
2015-01-21 20:14:31 -0800

    Slight modification to table schemas + UML
    Changed zip from integer to mediumint(5) and added "Other" as an option
    for subscriber type. UML created using UMLet

--- sql/tableCreation.sql
+++ sql/tableCreation.sql
@@ -1,15 +1,15 @@
 CREATE TABLE subscribers (
 	email varchar(255) NOT NULL,
 	name varchar(255) NOT NULL,
-	zip integer NOT NULL,
-	type enum ('Student', 'Alumni', 'Friend') NOT NULL,
+	zip mediumint(5) NOT NULL,
+	type enum ('Student', 'Alumni', 'Friend', 'Other') NOT NULL,
 	verificationDate datetime NULL,
 	PRIMARY KEY (email)
 );
 
 CREATE TABLE admins (
 	email varchar(255) NOT NULL,
-	password varchar(225) NOT NULL, -- Change this depending on
+	password varchar(225) NOT NULL, -- Change this depending on hashing algo
 	super boolean NOT NULL DEFAULT false,
 	salt varchar(255) NOT NULL, -- See above note
 	name varchar(255) NOT NULL,
