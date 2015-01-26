aa92cd3b5306d54532f81696b30b02f514b45140
cschadewitz <cschadewitz@eagles.ewu.edu>
2015-01-26 09:41:16 -0800

    Added component templetes to archived  news

---
+++ plugins/lasso/archivednews/components/SingleResult.php
@@ -0,0 +1,21 @@
+<?php namespace Lasso\ArchivedNews\Components;
+
+use Cms\Classes\ComponentBase;
+
+class SingleResult extends ComponentBase
+{
+
+    public function componentDetails()
+    {
+        return [
+            'name'        => 'SingleResult Component',
+            'description' => 'No description provided yet...'
+        ];
+    }
+
+    public function defineProperties()
+    {
+        return [];
+    }
+
+}
