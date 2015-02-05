4bd56b8924190962599c45ec342cf6db49d40c83
Samir Ouahhabi <samir@ouahhabi.com>
2015-02-05 00:26:10 -0800

    AdminSendMail plugin created - Create EditMail component - Create PreviewMail component

---
+++ plugins/lasso/adminsendmail/components/PreviewMail.php
@@ -0,0 +1,20 @@
+<?php namespace Lasso\AdminSendMail\Components;
+
+use Cms\Classes\ComponentBase;
+
+class PreviewMail extends ComponentBase
+{
+    public function componentDetails()
+    {
+        return [
+            'name' => 'Preview Mail',
+            'description' => 'Preview an email before sending it.'
+        ];
+    }
+
+    public function onRun()
+	{
+        $this->page['subject'] = post('subject');
+        $this->page['message'] = htmlspecialchars_decode(post('message'));
+	}
+}
