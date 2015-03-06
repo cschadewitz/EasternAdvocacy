9cf0934ee57be67520d7a03a53cc5a407b635945
dschultz1 <dschultz@eagles.ewu.edu>
2015-03-06 15:53:50 -0800

    Petitions Plugin
    Initial version of petitions plugin.
    Backend accessible at /lasso/petitions/petitions,
    /lasso/petitions/petitions/create, and /lasso/petitions/signatures
    1.0.1:
    - Added migrations
    - create_petitions_table.php
    - create_signatures_table.php
    1.0.5:
    - Added petitions and signatures models
    1.1.0:
    - Added seed
    - seed_petitions.php
    - seed_signatures.php
    1.2.0:
    - Added petitions controller
    - Added backend views
    - list, create, update, delete, preview behaviors
    1.2.1:
    - Bug fixes
    1.3.0:
    - Added signatures controller
    - Added backend views
    - list, update, delete, preview behaviors
    1.3.5:
    - Added signature relations to petitions controller
    - Added plugin permissions to Plugin.php
    - Added permission control to petitions & signatures controllers
    1.4.0:
    - Added component petitionPost
    - Basic front end complete

---
+++ plugins/lasso/petitions/components/Post.php
@@ -0,0 +1,87 @@
+<?php
+    namespace Lasso\Petitions\Components;
+
+    use Illuminate\Support\Facades\DB;
+
+    class Post extends \Cms\Classes\ComponentBase
+    {
+        public function componentDetails()
+        {
+            return [
+                'name' => 'Petition Posting',
+                'description' => 'Displays the front end representation of the petition to the end users.',
+            ];
+        }
+
+        public function defineProperties()
+        {
+            return [
+                'petition' => [
+                    'title' => 'Which petition',
+                    'type' => 'dropdown',
+                    'default' => 'Random Petition',
+                    'placeholder' => 'Select Petition'
+                ]
+            ];
+        }
+
+        public function getPetitionOptions()
+        {
+            $petitions = \Lasso\Petitions\Models\Petitions::all();
+            $result = [];
+
+            foreach ($petitions as $code=>$data)
+                $result[$data['pid']] = $data['title'];
+
+            return $result;
+        }
+
+        public function getPetition()
+        {
+            //$petition = \Lasso\Petitions\Models\Petitions::getPetition($this->property('petition'));
+            $petition = DB::table('petitions')->where('pid', $this->property('petition'))->first();
+            $result = [];
+            foreach($petition as $code=>$data)
+            {
+                $result[$code] = $data;
+            }
+            return $result;
+        }
+
+        public function signPetition(){
+            $name = post('name');
+            $email = post('email');
+            $mailingAddress = post('mailingaddress');
+            $city = post('city');
+            $zip = post('zip');
+            $pid = post('pid');
+
+            if ( empty($name) )
+                throw new \Exception(sprintf('Please enter your name.'));
+            if ( empty($email) )
+                throw new \Exception(sprintf('Please enter your email address.'));
+            if(empty($mailingAddress))
+                throw new \Exception(sprintf('Enter your registered mailing address'));
+            if ( empty($zip) )
+                throw new \Exception(sprintf('Please enter your zip code.'));
+            else{
+                $signature = new \lasso\petitions\models\Signatures;
+                $signature->name = $name;
+                $signature->email = $email;
+                $signature->zip = $zip;
+                $signature->address = $mailingAddress;
+                $signature->city = $city;
+                $signature->pid = $pid;
+                $signature->save();
+                $petition = \lasso\petitions\models\Petitions::pid($pid);
+                $petition->increment('signatures');
+              //  $petition->save();
+            }
+        }
+
+        public function onRun()
+        {
+            $this->page['petitionInfo'] = $this->getPetition();
+            $this->addCss('/plugins/lasso/petitions/assets/css/style.css');
+        }
+    }
---
+++ plugins/lasso/petitions/controllers/Petitions.php
@@ -0,0 +1,27 @@
+<?php
+    namespace Lasso\Petitions\Controllers;
+
+    use Backend\Facades\BackendMenu;
+
+    class Petitions extends \Backend\Classes\Controller
+    {
+        public $requiredPermissions = ['lasso.petitions.access_petitions'];
+
+        public $implement = ['Backend.Behaviors.FormController',
+                            'Backend.Behaviors.ListController',
+                            'Backend.Behaviors.RelationController',
+        ];
+
+        public $formConfig = 'form_config.yaml';
+
+        public $listConfig = 'list_config.yaml';
+
+        public $relationConfig = 'config_relation.yaml';
+
+        public function __construct()
+        {
+            parent::__construct();
+
+            BackendMenu::setContext('Lasso.Petitions', 'Petitions', 'View');
+        }
+    }
