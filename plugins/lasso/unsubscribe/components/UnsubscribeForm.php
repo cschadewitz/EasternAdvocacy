b5ffdc1017988c77e93c13ad68d1e58b41addd42
Zach Lesperance <zplesperance@gmail.com>
2015-03-07 14:05:45 -0800

    Add new unsubscribe component Preliminary commit

---
+++ plugins/lasso/unsubscribe/components/UnsubscribeForm.php
@@ -0,0 +1,30 @@
+<?php namespace Lasso\Unsubscribe\Components;
+
+use Cms\Classes\ComponentBase;
+
+class UnsubscribeForm extends ComponentBase
+{
+
+    public function componentDetails()
+    {
+        return [
+            'name'        => 'Unsubscribe Form',
+            'description' => 'No description provided yet...'
+        ];
+    }
+
+    public function defineProperties()
+    {
+        return [];
+    }
+
+    public function onRun()
+    {
+    }
+
+    public function onUnsubscribe()
+    {
+
+    }
+
+}
