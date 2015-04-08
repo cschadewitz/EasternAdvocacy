74c661d68b3e90724d335bb89ced812fe963af0e
Samir Ouahhabi <samir@ouahhabi.com>
2015-04-08 12:58:37 -0700

    Fix sendmail to disable sending emails already sent. Apply PSR-2 coding standards

--- plugins/lasso/adminsendmail/controllers/Emails.php
+++ plugins/lasso/adminsendmail/controllers/Emails.php
@@ -1,93 +1,97 @@
 <?php namespace Lasso\Adminsendmail\Controllers;
 
 use App;
-use Mail;
-use Redirect;
 use Backend;
 use BackendMenu;
-use Request;
 use Backend\Classes\Controller;
 use Lasso\Adminsendmail\Models\Email;
 use Lasso\Adminsendmail\Models\Subscriber;
-use System\Models\File;
 use Lasso\Adminsendmail\ReportWidgets\TopViewed;
+use Mail;
+use Redirect;
+use System\Models\File;
 
 /**
  * Emails Back-end Controller
  */
-class Emails extends Controller
-{
-    public $implement = [
-        'Backend.Behaviors.FormController',
-        'Backend.Behaviors.ListController'
-    ];
+class Emails extends Controller {
+	public $implement = [
+		'Backend.Behaviors.FormController',
+		'Backend.Behaviors.ListController',
+	];
 
-    public $formConfig = 'config_form.yaml';
-    public $listConfig = 'config_list.yaml';
+	public $formConfig = 'config_form.yaml';
+	public $listConfig = 'config_list.yaml';
 
-    public function __construct()
-    {
-        parent::__construct();
-        $this->assignVars();
-        $this->vars['postsTotal'] = Email::count();
-        $this->vars['popularPost'] = $this->popularPost;
-        BackendMenu::setContext('Lasso.Adminsendmail', 'email', 'emails');
-        $topViewedWidget = new TopViewed($this);
-        $topViewedWidget->alias = 'topViewed';
-        $topViewedWidget->bindToController();
-        $this->addCss('/plugins/lasso/adminsendmail/assets/css/box.css');
-        $this->addCss('/plugins/lasso/adminsendmail/assets/css/fileupload.css');
-    }
+	public function __construct() {
+		parent::__construct();
+		$this->assignVars();
+		$this->vars['postsTotal'] = Email::count();
+		$this->vars['popularPost'] = $this->popularPost;
+		BackendMenu::setContext('Lasso.Adminsendmail', 'email', 'emails');
+		$topViewedWidget = new TopViewed($this);
+		$topViewedWidget->alias = 'topViewed';
+		$topViewedWidget->bindToController();
+		$this->addCss('/plugins/lasso/adminsendmail/assets/css/box.css');
+		$this->addCss('/plugins/lasso/adminsendmail/assets/css/fileupload.css');
+	}
 
-    public function assignVars()
-    {
-        $this->popularPost = Email::orderBy('views', 'desc')->take(1)->get();
-    }
+	public function assignVars() {
+		$this->popularPost = Email::orderBy('views', 'desc')->take(1)->get();
+	}
 
-    public function onUpdate()
-    {
-        return Redirect::to(Backend::url('lasso/adminsendmail/emails/preview/'.$id));
-    }
+	public function onUpdate() {
+		return Redirect::to(Backend::url('lasso/adminsendmail/emails/preview/' . $id));
+	}
 
-    public function send($id = null)
-    {
-        $issent = true;
-        $email = Email::with('attachments')->find($id);
+	public function send($id = null) {
+		$issent = true;
+		$error = '';
+		$email = Email::with('attachments')->find($id);
 
-        if(is_null($email))
-            $issent = false;
+		if (is_null($email)) {
+			$issent = false;
+			$error = 'Invalid post id';
+		}
 
-        //send email
-        $subs = Subscriber::all();
-        $params = ['msg' => $email->content, 'subject' => $email->subject];
-        foreach($subs as $subscriber)
-        {
-            $params['unsubscribeUrl'] = '/unsubscribe/'.$subscriber->email.'/'.$subscriber->uuid;
-            Mail::send('lasso.adminsendmail::mail.default', $params, function($message) use ($subscriber, $email) {
-                $message->to($subscriber->email, $subscriber->name);
-                foreach($email->attachments as $attachment)
-                {
-                    $message->attach(App::basePath().$attachment->getPath());
-                }
-            });
-        } 
+		if ($email->sent == true) {
+			$issent = false;
+			$error = 'This email has already been sent';
+		}
 
-        $this->pageTitle = 'Send Email';
+		//send email
+		$subs = Subscriber::all();
+		$params = ['msg' => $email->content, 'subject' => $email->subject];
+		foreach ($subs as $subscriber) {
+			$params['unsubscribeUrl'] = '/unsubscribe/' . $subscriber->email . '/' . $subscriber->uuid;
+			Mail::send('lasso.adminsendmail::mail.default', $params, function ($message) use ($subscriber, $email) {
+				$message->to($subscriber->email, $subscriber->name);
+				foreach ($email->attachments as $attachment) {
+					$message->attach(App::basePath() . $attachment->getPath());
+				}
+			});
+		}
 
-        return $this->makePartial('send', [
-                'email' => $email,
-                'sent' => $issent,
-                'subs' => $subs
-            ]);
-    }
+		$email->sent = true;
+		$email->save();
 
-    public function download($id = null)
-    {
-        $file = File::find($id);
+		$this->pageTitle = 'Send Email';
 
-         if(is_null($file))
-            return Redirect::to(Backend::url('lasso/adminsendmail/emails'));
+		return $this->makePartial('send', [
+			'email' => $email,
+			'sent' => $issent,
+			'subs' => $subs,
+			'error' => $error,
+		]);
+	}
 
-        echo $file->output();
-    }
+	public function download($id = null) {
+		$file = File::find($id);
+
+		if (is_null($file)) {
+			return Redirect::to(Backend::url('lasso/adminsendmail/emails'));
+		}
+
+		echo $file->output();
+	}
 }
--- plugins/lasso/adminsendmail/models/Email.php
+++ plugins/lasso/adminsendmail/models/Email.php
@@ -2,44 +2,40 @@
 namespace Lasso\AdminSendMail\Models;
 
 use Model;
-use App;
 
-class Email extends Model 
-{
-    protected $table = 'lasso_adminsendmail_emails';
+class Email extends Model {
+	protected $table = 'lasso_adminsendmail_emails';
 
-    protected $fillable = ['id', 'subject', 'abstract', 'content'];
+	protected $fillable = ['id', 'subject', 'abstract', 'content'];
 
-    protected $visible =  ['id', 'subject', 'abstract', 'content', 'created_at', 'updated_at', 'views'];
+	protected $visible = ['id', 'subject', 'abstract', 'content', 'created_at', 'updated_at', 'views'];
 
-    public $hasOne = [];
-    public $hasMany = [];
-    public $belongsTo = [];
-    public $belongsToMany = [];
-    public $morphTo = [];
-    public $morphOne = [];
-    public $morphMany = [];
-    public $attachOne = [];
-    public $attachMany = [
-        'attachments' => ['System\Models\File']
-    ];
+	public $hasOne = [];
+	public $hasMany = [];
+	public $belongsTo = [];
+	public $belongsToMany = [];
+	public $morphTo = [];
+	public $morphOne = [];
+	public $morphMany = [];
+	public $attachOne = [];
+	public $attachMany = [
+		'attachments' => ['System\Models\File'],
+	];
 
-    public function scopeListPosts($query, $options)
-    {
-        extract(array_merge([
-            'page'       => 1,
-            'postsPerPage'    => 10,
-        ], $options));
+	public function scopeListPosts($query, $options) {
+		extract(array_merge([
+			'page' => 1,
+			'postsPerPage' => 10,
+		], $options));
 
-        return $query->paginate($postsPerPage, $page);
-    }
+		return $query->paginate($postsPerPage, $page);
+	}
 
-    public function setUrl($pageName, $controller)
-    {
-        $params = [
-            'id'    => $this->id,
-            'slug'  => $this->id,
-            ];
-        return $this->url = $controller->pageUrl($pageName, $params);
-    }
+	public function setUrl($pageName, $controller) {
+		$params = [
+			'id' => $this->id,
+			'slug' => $this->id,
+		];
+		return $this->url = $controller->pageUrl($pageName, $params);
+	}
 }
