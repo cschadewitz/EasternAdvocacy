96b6f55dbb7a96e0245bbc49188e090c43de04d4
Zach Lesperance <zplesperance@gmail.com>
2015-04-12 21:57:16 -0700

    Add Unsubscribe form to Unsubscribe component

--- plugins/lasso/unsubscribe/components/UnsubscribeForm.php
+++ plugins/lasso/unsubscribe/components/UnsubscribeForm.php
@@ -20,11 +20,32 @@ class UnsubscribeForm extends ComponentBase
 
     public function onRun()
     {
+        $this->addCss('/plugins/lasso/subscribe/assets/css/component-style.css');
     }
 
     public function onUnsubscribe()
     {
+        $email = post('email');
+        $zip = post('zip');
 
+        if ( empty($email) || empty($zip))
+            throw new \Exception(sprintf('Please enter your email address.'));
+        if ( empty($zip) )
+            throw new \Exception(sprintf('Please enter your zip code.'));
+
+        $user = Subscribe::whereRaw('email = ? and zip = ?', array($email, $zip));
+
+        if ($user->count() == 0) {
+            // Throw some error page
+            $this->success = false;
+            $this->message = "The entered email or zip did not match our records.";
+            return;
+        }
+
+        $user->delete();
+
+        $this->success = true;
+        $this->message = "You have been successfully unsubscribed from the mailing list.";
     }
 
 }
