e3e660f41d563380913e96238f57abe0be2e8784
Zach Lesperance <zplesperance@gmail.com>
2015-04-18 17:36:27 -0700

    Implement ReCaptcha for Unsubscribe form

--- plugins/lasso/unsubscribe/components/UnsubscribeForm.php
+++ plugins/lasso/unsubscribe/components/UnsubscribeForm.php
@@ -1,9 +1,11 @@
 <?php namespace Lasso\Unsubscribe\Components;
 
 use Cms\Classes\ComponentBase;
+use Symfony\Component\HttpKernel\Exception\HttpException;
 
 class UnsubscribeForm extends ComponentBase
 {
+    private $captchaSecret = '6LeBkgUTAAAAAPumAWlBZUCdoOGNdY8IugX6B3Ia';
 
     public function componentDetails()
     {
@@ -21,25 +23,39 @@ class UnsubscribeForm extends ComponentBase
     public function onRun()
     {
         $this->addCss('/plugins/lasso/subscribe/assets/css/component-style.css');
+        $this->addJs('https://www.google.com/recaptcha/api.js');
     }
 
     public function onUnsubscribe()
     {
         $email = post('email');
         $zip = post('zip');
+        $captcha = post('g-recaptcha-response');
 
         if ( empty($email) || empty($zip))
             throw new \Exception(sprintf('Please enter your email address.'));
         if ( empty($zip) )
             throw new \Exception(sprintf('Please enter your zip code.'));
+        if ( empty($captcha))
+            throw new \Exception(sprintf('Please enter the Captcha'));
+
+        $captchaRequest = new \HttpRequest('https://www.google.com/recaptcha/api/siteverify', HttpRequest::METH_POST);
+        $captchaRequest->addPostFields(array('secret'=>$this->captchaSecret, 'response'=>$captcha));
+
+        try {
+            $captchaResponse = json_decode($captchaRequest->send()->getBody());
+            if ($captchaResponse->success == false)
+                throw new \Exception(sprintf('Incorrect Captcha Response'));
+
+        } catch (HttpException $ex) {
+            throw new \Exception(sprintf('Could not verify Captcha'));
+        }
 
         $user = Subscribe::whereRaw('email = ? and zip = ?', array($email, $zip));
 
         if ($user->count() == 0) {
             // Throw some error page
-            $this->success = false;
-            $this->message = "The entered email or zip did not match our records.";
-            return;
+            throw new \Exception(sprintf("The entered email or zip did not match our records."));
         }
 
         $user->delete();
