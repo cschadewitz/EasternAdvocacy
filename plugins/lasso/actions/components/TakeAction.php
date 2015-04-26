471be4709f896559130bb9525e1ba95482cb09b2
Samir Ouahhabi <samir@ouahhabi.com>
2015-04-25 19:56:20 -0700

    Add a few functions to implement better geolocating of the Reps in the TakeAction component

--- plugins/lasso/actions/components/TakeAction.php
+++ plugins/lasso/actions/components/TakeAction.php
@@ -26,24 +26,38 @@ class TakeAction extends ComponentBase {
 	public function onRun() {
 		$zip = new Zip;
 		$this->addCss('/plugins/lasso/actions/assets/css/frontend.css');
+		$this->addJs('/plugins/lasso/actions/assets/js/frontend.js');
 		$action = Action::with('template')->find($this->property('actionId'));
 		$this->page['action'] = $action;
-		$this->page['reps'] = $this->lookupReps('99004'); //$zip->info('99004');
+		$this->page['reps'] = $this->lookupReps('eastern washington university'); //$zip->info('99004');
 		//$this->page['debug'] = $this->lookupReps('99004');
 	}
 
 	public function lookupReps($address) {
-		$geokey = 'AIzaSyAl4hlRsBUmjLtxvFC7O_gBJfw1Ow1pfMg';
-		$openhousekey = 'f78ae693f1c343aab4d0e5898eca594d';
+		$geo = $this->getGeoCode($address);
+
+		$reps = $this->getGeoReps($geo);
+
+		return $reps;
+	}
+
+	public function getGeoCode($address) {
+		$geokey = 'AIzaSyA9spIfrAzZZy84WCQQ_WxQozHHNiupVbE';
 
 		$json = file_get_contents(sprintf(
 			"https://maps.googleapis.com/maps/api/geocode/json?address=%s&key=%s",
-			$address,
+			urlencode($address),
 			$geokey
 		));
-		$geo = json_decode($json)->{'results'};
-		$lat = $geo->geometry->location->lat; // 46.486931;
-		$long = $geo->geometry->location->long; // -107.575956;
+
+		return json_decode($json, true);
+	}
+
+	public function getGeoReps($geo) {
+		$openhousekey = 'f78ae693f1c343aab4d0e5898eca594d';
+
+		$lat = $geo['results'][0]['geometry']['location']['lat']; // 46.486931;
+		$long = $geo['results'][0]['geometry']['location']['lng']; // -107.575956;
 
 		$json = file_get_contents(sprintf(
 			"http://openstates.org/api/v1//legislators/geo/?lat=%s&long=%s&apikey=%s",
@@ -53,7 +67,5 @@ class TakeAction extends ComponentBase {
 		));
 
 		return json_decode($json);
-
 	}
-
 }
