a0d90d0f2a71c9048d02aa7a1dce9b8b3c916542
DanAldous <danaldous@gmail.com>
2015-04-27 14:06:32 -0700

    Further tweaks, mostly refactored, doesn't work at this time (is the ajax callback even being called?) Models refactored so that the tables are created now Moved most logic into respective models

--- plugins/lasso/legislativelookup/models/Address.php
+++ plugins/lasso/legislativelookup/models/Address.php
@@ -21,7 +21,7 @@ class Address extends Model
     /**
      * @var array Fillable fields
      */
-    protected $fillable = [];
+    protected $fillable = ['address','city','state','zip','lat','long','district'];
 
     /**
      * @var array Relations
@@ -37,29 +37,62 @@ class Address extends Model
     public $attachMany = [];
 
     /**
-     * @param $address - Array of address components
-     * @return mixed - JSON containing the lat/long coordinates - might normalize here
+     * @param $street_address - Array of address components
+     * @return Address - new address object with lat/long coordinates looked up
      */
-    public function coordinatesFromAddress($street_address) {
+    public static function parseNewAddress($street_address) {
         $address = $street_address[0];
         $city = $street_address[1];
         $state = $street_address[2];
         $zip = $street_address[3];
 
-        $street = ($address==null?("address=" . $address):"");
-        $street = $street . ($city==null?("city=" . $city):"");
-        $street = $street . ($state==null?("state=" . $state):"");
-        $street = $street . ($zip==null?("zip=" . $zip):"");
+        $street = "address=";
+        $street = $street . ($address==null?$address:"");
+        $street = $street . ($city==null?$city:"");
+        $street = $street . ($state==null?$state:"");
+        $street = $street . ($zip==null?$zip:"");
+
+        echo $street;
+        printf("%s", $street);
+
         $json = file_get_contents(sprintf(
             "https://maps.googleapis.com/maps/api/geocode/json?%s&sensor=false&key=%s",
             $street,
             Settings::get('google_id')
-        //$this->property('userGoogleID')
         ));
         $json = json_decode($json)->{'results'};
         $json = json_decode($json)->{'geometry'};
         $json = json_decode($json)->{'location'};
-        return json_decode($json);
+        $lat=round(json_decode($json)->{'lat'}, 1);//normalized here
+        $long=round(json_decode($json)->{'lng'}, 1);
+        $newAddress = Address::create([
+            'address' => $address,
+            'city' => $city,
+            'state' => $state,
+            'zip' => $zip,
+            'lat' => $lat,
+            'long' => $long
+        ]);
+        $newAddress->save();
+
+        echo $newAddress;
+
+        return $newAddress;
     }
 
+    public function getLat(){
+        return round($this->lat, 1);//normalize here
+    }
+    public function getLong(){
+        return round($this->long, 1);
+    }
+    public function scopeDistrict($query, $district){
+        return $query->where('district', '=', $district);
+    }
+    public function scopeState($query, $state){
+        return $query->where('state', '=', $state);
+    }
+    public function scopeZip($query, $zip){
+        return $query->where('zip', '=', $zip);
+    }
 }
