9b2269272aa506897505615ddf5d2b6bc334bbd0
cschadewitz <cschadewitz@eagles.ewu.edu>
2015-05-02 23:59:26 -0700

    Facebook Integration

---
+++ plugins/lasso/social/Plugin.php
@@ -0,0 +1,51 @@
+<?php namespace Lasso\Social;
+
+use System\Classes\PluginBase;
+use Lasso\Social\Models\Settings;
+
+/**
+ * FacebookIntegration Plugin Information File
+ */
+class Plugin extends PluginBase
+{
+
+    /**
+     * Returns information about this plugin.
+     *
+     * @return array
+     */
+    public function pluginDetails()
+    {
+        return [
+            'name'        => 'Social',
+            'description' => '',
+            'author'      => 'Lasso',
+            'icon'        => 'icon-rss-square'
+        ];
+    }
+
+    public function registerSettings()
+    {
+        return [
+            'social' => [
+                'label'       => 'Social',
+                'description' => 'Manage Social api keys & urls',
+                'category'    => 'Social',
+                'icon'        => 'icon-rss-square',
+                'class'       => 'Lasso\Social\Models\Settings',
+                'order'       => 500,
+                'keywords'    => 'facebook'
+            ]
+        ];
+    }
+
+    public function registerComponents()
+    {
+        return [
+            'Lasso\Social\Components\FacebookIntegration' => 'integration',
+            'Lasso\Social\Components\FacebookShareDialog' => 'share_dialog',
+            'Lasso\Social\Components\SocialButtons'       => 'social_buttons'
+        ];
+    }
+
+}
