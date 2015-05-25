a8bc3a5cf6a6a1c53637273d87e74bd0e012cf25
cschadewitz <cschadewitz@eagles.ewu.edu>
2015-05-25 11:11:19 -0700

    Removed un-necessary eager loading

--- plugins/lasso/subscribe/models/UserExtension.php
+++ plugins/lasso/subscribe/models/UserExtension.php
@@ -38,9 +38,7 @@ class UserExtension extends Model
 
     public function scopeSubscribers($query, $inverse = false)
     {
-        $temp = $query->with('user')->whereHas('user', function($userQuery){
-            $userQuery->where('users.isActivated', '=', 1);
-        });
+        $temp = $query->where('users.isActivated', '=', 1);
         if(!$inverse)
             return $temp->where('verificationDate', 'IS NOT', 'NULL');
         else
