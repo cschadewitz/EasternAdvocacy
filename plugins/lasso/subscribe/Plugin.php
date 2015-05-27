e5e998eaa802019055c172f3bc2c8f90625fbe12
cschadewitz <cschadewitz@eagles.ewu.edu>
2015-05-27 05:30:25 -0700

    try key change

--- plugins/lasso/subscribe/Plugin.php
+++ plugins/lasso/subscribe/Plugin.php
@@ -66,7 +66,7 @@
         protected function extendUserModel()
         {
             UserModel::extend(function ($model) {
-                $model->hasOne['extension'] = ['Lasso\Subscribe\Models\UserExtension', 'table' => 'lasso_subscribe_user_extensions', 'foreign_key' => 'user_id'];
+                $model->hasOne['extension'] = ['Lasso\Subscribe\Models\UserExtension', 'table' => 'lasso_subscribe_user_extensions', 'key' => 'user_id'];
                 //$model->visible = ['id'];
                 //UserExtension::getModel($model);
             });
