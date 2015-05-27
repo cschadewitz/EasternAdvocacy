be957082834df60cb3f9350fc98444f9a5a46b1f
cschadewitz <cschadewitz@eagles.ewu.edu>
2015-05-27 14:55:30 -0700

    fix

--- plugins/lasso/subscribe/components/UserSubscribe.php
+++ plugins/lasso/subscribe/components/UserSubscribe.php
@@ -147,7 +147,7 @@ class UserSubscribe extends ComponentBase
         if ($automaticActivation || !$requireActivation) {
             Auth::login($user);
         }
-        $subbed = ($data['subscribe'] ? date('Y-m-d H:i:s') : null);
+        $subbed = (array_key_exists('subscribe', $data) ? date('Y-m-d H:i:s') : null);
 
         UserExtension::getModel($user, $subbed, $data['affiliation']);
 
