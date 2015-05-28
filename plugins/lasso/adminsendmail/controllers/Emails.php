1c81e2b95a675544106d7b454753dec6cd1c7e54
Samir Ouahhabi <samir@ouahhabi.com>
2015-05-28 17:48:16 -0400

    Refactor SendMail plugin

--- plugins/lasso/adminsendmail/controllers/Emails.php
+++ plugins/lasso/adminsendmail/controllers/Emails.php
@@ -6,6 +6,7 @@ use BackendMenu;
 use Backend\Classes\Controller;
 use Lasso\Adminsendmail\Models\Email;
 use Lasso\Subscribe\Models\Subscribe;
+use Lasso\Subscribe\Models\UserExtension;
 use Lasso\Adminsendmail\ReportWidgets\TopViewed;
 use Mail;
 use Redirect;
@@ -25,93 +26,114 @@ class Emails extends Controller {
 
 	public function __construct() {
 		parent::__construct();
+
 		$this->assignVars();
-		$this->vars['postsTotal'] = Email::count();
-		$this->vars['popularPost'] = $this->popularPost;
-		BackendMenu::setContext('Lasso.Adminsendmail', 'email', 'emails');
-		$topViewedWidget = new TopViewed($this);
-		$topViewedWidget->alias = 'topViewed';
-		$topViewedWidget->bindToController();
-		$this->addCss('/plugins/lasso/adminsendmail/assets/css/box.css');
-		$this->addCss('/plugins/lasso/adminsendmail/assets/css/fileupload.css');
+
+        $this->initWidgets();
+
+        $this->injectAssets();
+
+        BackendMenu::setContext('Lasso.Adminsendmail', 'email', 'emails');
 	}
+
+    private function initWidgets()
+    {
+        //top viewed widget
+        $topViewedWidget = new TopViewed($this);
+        $topViewedWidget->alias = 'topViewed';
+        $topViewedWidget->bindToController();
+    }
+
+    private function injectAssets()
+    {
+        $this->addCss('/plugins/lasso/adminsendmail/assets/css/box.css');
+        $this->addCss('/plugins/lasso/adminsendmail/assets/css/fileupload.css');
+    }
 
 	public function assignVars() {
 		$this->popularPost = Email::orderBy('views', 'desc')->take(1)->get();
-	}
-
-	public function onUpdate() {
-		return Redirect::to(Backend::url('lasso/adminsendmail/emails/preview/' . $id));
+        $this->vars['postsTotal'] = Email::count();
+        $this->vars['popularPost'] = $this->popularPost;
 	}
 
 	public function send($id = null) {
 		$this->pageTitle = 'Send Email';
-		$issent = true;
-		$error = '';
+        $subs = Subscribe::verified()->get();
+        $users = UserExtension::subscribers()->get();
+
 		$email = Email::with('attachments')->find($id);
 
-		if (is_null($email)) {
-			$issent = false;
-			$error = 'Invalid post id';
+        if($this->canSend($email)) {
+            $this->sendEmail($email);
 
-			return $this->makePartial('send', [
-				'email' => $email,
-				'sent' => $issent,
-				'error' => $error,
-			]);
-		}
-
-		if ($email->sent == true) {
-			$issent = false;
-			$error = 'This email has already been sent';
-
-			return $this->makePartial('send', [
-				'email' => $email,
-				'sent' => $issent,
-				'error' => $error,
-			]);
-		}
-
-		//send email
-		$subs = Subscriber::verified()->get();
-		$users = UserExtension::subscribers()->get();
-		$params = ['msg' => $email->content, 'subject' => $email->subject];
-		foreach ($subs as $subscriber) {
-			$params['unsubscribeUrl'] = '/unsubscribe/' . $subscriber->email . '/' . $subscriber->uuid;
-			Mail::send('lasso.adminsendmail::mail.default', $params, function ($message) use ($subscriber, $email) {
-				$message->to($subscriber->email, $subscriber->name);
-				foreach ($email->attachments as $attachment) {
-					$message->attach(App::basePath() . $attachment->getPath());
-				}
-			});
-		}
-		foreach ($users as $user) {
-			$params['loginUrl'] = '/user/account/';
-			Mail::send('lasso.adminsendmail::mail.user', $params, function ($message) use ($user, $email) {
-				$message->to($user->user->email, $user->user->name);
-				foreach ($email->attachments as $attachment) {
-					$message->attach(App::basePath() . $attachment->getPath());
-				}
-			});
-		}
-
-		$email->sent = true;
-		$email->save();
-
-		return $this->makePartial('send', [
-			'email' => $email,
-			'sent' => $issent,
-			'error' => $error,
-		]);
+            return $this->makePartial('send', [
+                'sent' => true,
+                'subs' => $subs,
+                'users' => $users]);
+        }
+        else {
+            return $this->makePartial('send', [
+                'sent' => false,
+                'error' => $this->getSendError($email),
+                'subs' => $subs,
+                'users' => $users
+            ]);
+        }
 	}
 
-	public function download($id = null) {
-		$file = File::find($id);
+    private function sendEmail($email)
+    {
+        $params = ['msg' => $email->content, 'subject' => $email->subject, 'unsubscribeUrl' => ''];
 
-		if (is_null($file)) {
-			return Redirect::to(Backend::url('lasso/adminsendmail/emails'));
-		}
+        $this->emailSubs($params, $email);
 
-		echo $file->output();
-	}
+        $this->emailUsers($params, $email);
+
+        $email->sent = true;
+        $email->save();
+    }
+
+    private function emailSubs($params, $email)
+    {
+        $subs = Subscribe::verified()->get();
+
+        foreach ($subs as $subscriber) {
+            $params['unsubscribeUrl'] = '/unsubscribe/' . $subscriber->email . '/' . $subscriber->uuid;
+            Mail::send('lasso.adminsendmail::mail.default', $params, function ($message) use ($subscriber, $email) {
+                $message->to($subscriber->email, $subscriber->name);
+                foreach ($email->attachments as $attachment) {
+                    $message->attach(App::basePath() . $attachment->getPath());
+                }
+            });
+        }
+    }
+
+    private function emailUsers($params, $email)
+    {
+        $users = UserExtension::subscribers()->get();
+
+        foreach ($users as $user) {
+            $params['unsubscribeUrl'] = '/user/account/';
+            Mail::send('lasso.adminsendmail::mail.user', $params, function ($message) use ($user, $email) {
+                $message->to($user->user->email, $user->user->name);
+                foreach ($email->attachments as $attachment) {
+                    $message->attach(App::basePath() . $attachment->getPath());
+                }
+            });
+        }
+    }
+
+    private function getSendError($email)
+    {
+        if (is_null($email))
+            return 'Invalid post id';
+
+        if ($email->sent)
+            return 'This email has already been sent';
+    }
+
+    private function canSend($email)
+    {
+        return !is_null($email) && !($email->sent);
+    }
 }
