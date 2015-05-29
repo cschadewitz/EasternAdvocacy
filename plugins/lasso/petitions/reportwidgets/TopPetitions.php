17a2b14b52a5e61ec2e67fb3ee782ff63f3ac251
cschadewitz <cschadewitz@eagles.ewu.edu>
2015-05-29 14:18:35 -0700

    another test

--- plugins/lasso/petitions/reportwidgets/TopPetitions.php
+++ plugins/lasso/petitions/reportwidgets/TopPetitions.php
@@ -56,7 +56,7 @@ class TopPetitions extends ReportWidgetBase {
         $this->vars['numberOfPetitions'] = $this->numberOfPetitions = $this->property('numberOfPetitions');
         $this->vars['percentage'] = $this->percentage = $this->property('percentage');
         if($this->percentage) {
-            $this->vars['petitions'] = Petitions::sortByProgress()->take($this->numberOfPetitions)->get();
+            //$this->vars['petitions'] = Petitions::sortByProgress()->take($this->numberOfPetitions)->get();
         }
         else {
             $this->vars['petitions'] = Petitions::sortBySigCount()->take($this->numberOfPetitions)->get();
